270aee5b8c5114ee2476f793506b2477d492843b
keundeok2 <keundeok2@gmail.com>
2021-09-10 14:36:03 +0900

    spring validation FieldError, ObjectError

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -45,7 +45,7 @@ public class ValidationItemControllerV2 {
         return "validation/v2/addForm";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         /*
         @ModelAttribute -> Model에 자동으로 setAttribute
@@ -89,6 +89,53 @@ public class ValidationItemControllerV2 {
             if (resultPrice < 10000) {
                 // globalError일 경우 -> ObjectError 사용
                 bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("bindingResult = {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        /*
+        field의 type이 달라 binding에 실패할 경우에는 spring FieldError를 생성하여 해당 field의 값을 저장한다.
+        그 이후 controller method를 호출하여 BindingResult에 FieldError를 담는다.
+        FieldError의 두번째 생성자로 객체를 생성하기 때문에 이전의 값을 뷰에 전달 할 수 있다.
+         */
+
+
+
+        // 검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            // 하나의 특정 필드에 대한 에러 -> FieldError 사용, param (objectName: Model에 담기는 object)
+            // FieldError 두번째 생성자를 사용하면 이전의 값을 유지하여 뷰에 전달할 수 있음
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
+        }
+
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null, "가격은 1,000 ~ 1,000,000 까지 허용합니다."));
+        }
+
+        if ( item.getQuantity() == null || item.getQuantity() > 9999) {
+            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null, "수량은 최대 9,999 까지 허용합니다."));
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                // globalError일 경우 -> ObjectError 사용
+                bindingResult.addError(new ObjectError("item", null, null, "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
             }
         }
 
