789347761fe7bddd4bebdcc99b08e879df2d9b83
keundeok2 <keundeok2@gmail.com>
2021-09-18 19:57:39 +0900

    spring bean validation 10~11

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -17,26 +17,34 @@ public class Item {
     같은 모델 객체를 사용하는 등록과 수정에서, 검증 로직이 서로 다를 경우에는 어떻게 할 것인지?
     ->  1. groups 사용
     -> 실무에서 주로 사용 2. Item을 직접 사용하지 않고 ItemSaveForm, ItemUpdateForm과 같이 별도의 모델객체를 만들어 각각 사용
+
      */
 
-    @NotNull(groups = UpdateCheck.class)
+//    @NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class}) // 빈 값 혹은 공백만 있는 경우를 허용 X
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class}) // 빈 값 혹은 공백만 있는 경우를 허용 X
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class}) // null 허용 X
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class}) // 범위 안의 값만 허용
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class}) // null 허용 X
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class}) // 범위 안의 값만 허용
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = SaveCheck.class) // 최대 9999까지 허용
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = SaveCheck.class) // 최대 9999까지 허용
     private Integer quantity;
 
     public Item() {
     }
 
     public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Item(long id, String itemName, Integer price, Integer quantity) {
+        this.id = id;
         this.itemName = itemName;
         this.price = price;
         this.quantity = quantity;
