851df1974cf1c339ee761a4e2d4a325348394339
keundeok2 <keundeok2@gmail.com>
2021-09-23 20:59:31 +0900

    cookie session 서블릿HTTP세션 1~2

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -47,9 +49,9 @@ public class HomeController {
 
         model.addAttribute("member", loginMember);
         return "loginHome";
-
     }
-    @GetMapping("/")
+
+//    @GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model) {
 
         // 세션관리자에 저장된 회원정보 가져오기
@@ -61,7 +63,48 @@ public class HomeController {
 
         model.addAttribute("member", member);
         return "loginHome";
+    }
 
+//    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model) {
+
+        // 파라미터 false -> 로그인 하지않고 단순히 홈으로 들어온 사용자도 세션이 생성되기 때문
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
+
+        Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // 세션에 값이 없으면 홈으로 이
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인 홈으로 이동동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+        // @SessionAttribute는 세션을 새로 생성하지는 않는다.
+        /*
+        브라우저를 실행하고 최초 로그인 시 url끝에 jsessionid querystring이 붙는 이유
+        -> 웹 브라우저가 쿠키를 지원하지 않는 경우 url을 통해 session을 유지시키는 방법
+        -> 현실적으로 사용 불가능
+        -> 사용 중지: server.servlet.session.tracking-modes=cookie
+         */
+
+
+        // 세션에 값이 없으면 홈으로 이동
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인 홈으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 
 }
---
+++ login/src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,10 @@
+package hello.login.web;
+
+public class SessionConst {
+    public static final String LOGIN_MEMBER = "loginMember";
+
+    /*
+    단순히 스태틱 상수만을 가져다 쓰는 Class는 absract class 또는 interface로
+    -> 인스턴스가 생성되지 않는다
+     */
+}
