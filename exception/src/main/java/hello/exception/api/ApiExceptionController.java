b8b893fdbdc247b6ba753d93d84bc4f74d23d7a7
keundeok2 <keundeok2@gmail.com>
2021-10-04 20:14:02 +0900

    스프링이 제공하는 ExceptionResolver 1

--- exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,12 +1,15 @@
 package hello.exception.api;
 
+import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -42,6 +45,19 @@ public class ApiExceptionController {
 
      */
 
+
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
+    }
+
+    /**
+     * 개발자가 직접 수정할 수 없어 @ResponseStatus를 붙이지 못하는 Exception의 경우에는 ResponseStatusException을 생성해서 return
+     */
+    @GetMapping("/api/response-status-ex2")
+    public String responseStatusEx2() {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+    }
 
     @Data
     @AllArgsConstructor
